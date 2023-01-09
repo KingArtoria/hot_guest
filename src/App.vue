290d66ef0055717d736a8c057446eda406aa5071
KingArtoria <gaojiuan@163.com>
2023-01-09 17:59:04 +0800

    退出异常bug

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,7 @@ export default {
   },
   onLaunch() {
     // 声明全局版本号变量
-    Vue.prototype._version = "5.1.0";
+    Vue.prototype._version = "5.1.1";
     // 声明全局来源
     Vue.prototype._source = "ios";
     // 声明全局类型
